e686ba5653b6bbd88f05179bdb947b467fe0884f
Jairo Sousa <58406697+JairoSSantos@users.noreply.github.com>
2021-05-20 18:46:28 -0300

    criado scripts base
    criado o script "data.py" para controlar os dados e o script "main.py" para implementar a interface

---
+++ main.py
@@ -0,0 +1,11 @@
+import tkinter as tk
+import data
+
+class App:
+    def __init__(self, root):
+        pass
+
+if __name__ == '__main__':
+    root = tk.Tk()
+    app = App(root)
+    root.mainloop()
