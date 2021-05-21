4bacd09febf4d58988508c286a372a3e83a2e855
Jairo Sousa <58406697+JairoSSantos@users.noreply.github.com>
2021-05-21 11:48:39 -0300

    adicionado índice aos itens

--- data.py
+++ data.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from random import randint
 
 AUTOSAVE = True # salvamento altomátivo ativado com padrão
 STORAGE_FILENAME = 'storage.csv' # caminho até o arquivo de estoque
@@ -15,24 +16,9 @@ def set_autosave(autosave=True):
     global AUTOSAVE
     AUTOSAVE = autosave
 
-def storage_save():
-    '''
-    Salvar os dados do estoque.
-    '''
-    global STORAGE_DATA
-    STORAGE_DATA.to_csv(STORAGE_FILENAME, index=False)
-
-
-def storage_update():
-    '''
-    Atualizar a variável STORAGE_DATA.
-    '''
-    global STORAGE_DATA
-    STORAGE_DATA = pd.read_csv(STORAGE_FILENAME)
-
 def storage_add(nome:str, p_venda:float, p_custo:float, quantidade:int):
     '''
-    Adicionas item ao estoque.
+    Adicionar item ao estoque.
 
     Args:
         nome: nome do produto (type= str).
@@ -41,10 +27,32 @@ def storage_add(nome:str, p_venda:float, p_custo:float, quantidade:int):
         quantidade: quantidade disponível no estoque (type= int).
     '''
     global STORAGE_DATA, AUTOSAVE
+    index = 0
+    while index in list(STORAGE_DATA['id']): index += 1
     STORAGE_DATA = STORAGE_DATA.append({
+        'id': index,
         'nome':nome, 
         'p_venda':p_venda, 
         'p_custo':p_custo, 
         'quantidade':quantidade
     }, ignore_index=True)
     if AUTOSAVE: storage_save()
+
+def storage_save():
+    '''
+    Salvar os dados do estoque.
+    '''
+    global STORAGE_DATA
+    STORAGE_DATA.to_csv(STORAGE_FILENAME, index=False)
+
+def storage_update():
+    '''
+    Atualizar a variável STORAGE_DATA.
+    '''
+    global STORAGE_DATA
+    STORAGE_DATA = pd.read_csv(STORAGE_FILENAME)
+
+init()
+for i in range(10):
+    pre = randint(10, 20)
+    storage_add(f'item {i+1}', pre, pre-2, randint(0, 100))
