ad020b99959867c3e488a15b94c27514edfbf011
Jairo Sousa <58406697+JairoSSantos@users.noreply.github.com>
2021-05-21 21:13:27 -0300

    criado a classe Storage e o decorator autosave
    - Storage: criado uma classe para facilitar a manipulação dos dados do estoque
    - autosave: decorator para salvamento automático dos métodos que modificam algum dataframe
    - DataError: para tratar os erros

--- data.py
+++ data.py
@@ -1,58 +1,68 @@
 import pandas as pd
 from random import randint
 
-AUTOSAVE = True # salvamento altomátivo ativado com padrão
 STORAGE_FILENAME = 'storage.csv' # caminho até o arquivo de estoque
-STORAGE_DATA = None # variável para o dataframe
+
+def autosave(function):
+    def dec_function(*args, **kwargs):
+        function(*args, **kwargs)
+        args[0].save()
+    return dec_function
+
+class DataError(Exception): # classe de erros
+    def __init__(self, message):
+        super().__init__(message)
+
+class Storage:
+    def __init__(self, filename):
+        self.filename = filename
+        self.update()
+    
+    @autosave
+    def add(self, nome:str, p_venda:float, p_custo:float, quantidade:int):
+        '''
+        Adicionar item ao estoque.
+
+        Args:
+            nome: nome do produto (type= str).
+            p_venda: preço de venda do produto (type= float).
+            p_custo: preço de custo do produto (type= float).
+            quantidade: quantidade disponível no estoque (type= int).
+        '''
+        if nome in self.dataframe['nome'].tolist():
+            raise DataError('Nome já cadastrado.')
+        index = 0
+        while index in self.dataframe['id'].tolist(): index += 1
+        self.dataframe = self.dataframe.append({
+            'id': index,
+            'nome':nome, 
+            'p_venda':p_venda, 
+            'p_custo':p_custo, 
+            'quantidade':quantidade
+        }, ignore_index=True)
+    
+    @autosave
+    def delete(self, index):
+        '''
+        Deletar produto.
+
+        Args:
+            index: índice do produto a ser deletado.
+        '''
+        self.dataframe = self.dataframe.drop(index=index)
+    
+    def save(self):
+        '''
+        Salvar os dados.
+        '''
+        self.dataframe.to_csv(self.filename, index=False)
+    
+    def update(self):
+        '''
+        Atualizar o dataframe.
+        '''
+        self.dataframe = pd.read_csv(self.filename)
 
 def init():
-    storage_update()
-
-def set_autosave(autosave=True):
-    '''
-    Definir salvamento automático.
-    -> Se True, então sempre que algum dataframe for modificado será salvo.
-    '''
-    global AUTOSAVE
-    AUTOSAVE = autosave
-
-def storage_add(nome:str, p_venda:float, p_custo:float, quantidade:int):
-    '''
-    Adicionar item ao estoque.
-
-    Args:
-        nome: nome do produto (type= str).
-        p_venda: preço de venda do produto (type= float).
-        p_custo: preço de custo do produto (type= float).
-        quantidade: quantidade disponível no estoque (type= int).
-    '''
-    global STORAGE_DATA, AUTOSAVE
-    index = 0
-    while index in list(STORAGE_DATA['id']): index += 1
-    STORAGE_DATA = STORAGE_DATA.append({
-        'id': index,
-        'nome':nome, 
-        'p_venda':p_venda, 
-        'p_custo':p_custo, 
-        'quantidade':quantidade
-    }, ignore_index=True)
-    if AUTOSAVE: storage_save()
-
-def storage_save():
-    '''
-    Salvar os dados do estoque.
-    '''
-    global STORAGE_DATA
-    STORAGE_DATA.to_csv(STORAGE_FILENAME, index=False)
-
-def storage_update():
-    '''
-    Atualizar a variável STORAGE_DATA.
-    '''
-    global STORAGE_DATA
-    STORAGE_DATA = pd.read_csv(STORAGE_FILENAME)
-
-init()
-for i in range(10):
-    pre = randint(10, 20)
-    storage_add(f'item {i+1}', pre, pre-2, randint(0, 100))
+    global storage
+    storage = Storage(STORAGE_FILENAME)
