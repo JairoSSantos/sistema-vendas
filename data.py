6792baabc1d3cd52902c1aaa88a5ea553268dd72
Jairo Sousa <58406697+JairoSSantos@users.noreply.github.com>
2021-05-22 11:58:05 -0300

    melhoria na identificação dos itens

--- data.py
+++ data.py
@@ -4,9 +4,9 @@ from random import randint
 STORAGE_FILENAME = 'storage.csv' # caminho até o arquivo de estoque
 
 def autosave(function):
-    def dec_function(*args, **kwargs):
-        function(*args, **kwargs)
-        args[0].save()
+    def dec_function(self, *args, **kwargs):
+        function(self, *args, **kwargs)
+        self.save()
     return dec_function
 
 class DataError(Exception): # classe de erros
@@ -14,12 +14,24 @@ class DataError(Exception): # classe de erros
         super().__init__(message)
 
 class Storage:
+    '''
+    Classe para controlar os dados do estoque.
+        -> Os dados são armazenados em um dataframe do pandas.
+        -> Cada ítem cadastrado deve ter id e nome diferendes.
+        -> Colunas:
+            |- id: A principal idendificação do produto (não confundir com o index do dataframe, o id do produto não deve mudar)
+            |- nome: Nome do produto (não podem haver dois produtos com o mesmo nome)
+            |- p_venda: Preço de venda do produto.
+            |- p_custo: Preço que esse produto custou (este dado irá permitir o cálculo do lucro na venda do produto).
+            |- quantidade: Quantidade do produto em estoque.
+            |- descricao: Uma descrição sobre o produto.
+    '''
     def __init__(self, filename):
         self.filename = filename
         self.update()
     
     @autosave
-    def add(self, nome:str, p_venda:float, p_custo:float, quantidade:int):
+    def add(self, nome, p_venda, p_custo, quantidade=0, descricao=''):
         '''
         Adicionar item ao estoque.
 
@@ -31,25 +43,27 @@ class Storage:
         '''
         if nome in self.dataframe['nome'].tolist():
             raise DataError('Nome já cadastrado.')
-        index = 0
-        while index in self.dataframe['id'].tolist(): index += 1
+        id_item = 0
+        while id_item in self.dataframe['id'].tolist(): id_item += 1
         self.dataframe = self.dataframe.append({
-            'id': index,
+            'id': id_item,
             'nome':nome, 
             'p_venda':p_venda, 
             'p_custo':p_custo, 
-            'quantidade':quantidade
+            'quantidade':quantidade,
+            'descricao':descricao
         }, ignore_index=True)
     
     @autosave
-    def delete(self, index):
+    def delete(self, id_item):
         '''
         Deletar produto.
 
         Args:
-            index: índice do produto a ser deletado.
+            id_item: id do produto a ser deletado.
         '''
-        self.dataframe = self.dataframe.drop(index=index)
+        real_index = self.dataframe.loc[self.dataframe['id'] == id_item].index.item()
+        self.dataframe = self.dataframe.drop(index=real_index)
     
     def save(self):
         '''
@@ -64,5 +78,8 @@ class Storage:
         self.dataframe = pd.read_csv(self.filename)
 
 def init():
+    '''
+    Cria variáveis globais para acessar as classes de contole de dados.
+    '''
     global storage
     storage = Storage(STORAGE_FILENAME)
