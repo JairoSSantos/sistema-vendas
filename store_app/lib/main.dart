9e17cf9b9a6a9db5af0320aac88dce5d54625196
Jairo Sousa <58406697+JairoSSantos@users.noreply.github.com>
2024-01-30 16:37:49 -0300

    MySql connection login

--- store_app/lib/main.dart
+++ store_app/lib/main.dart
@@ -1,4 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:window_manager/window_manager.dart';
+import 'package:mysql1/mysql1.dart';
+
+final loginLabels = {
+  'user': 'Usuário',
+  'password': 'Senha'
+};
 
 final fields = {
   'code': 'Código',
@@ -8,21 +15,110 @@ final fields = {
   'quantity': 'Quantidade'
 };
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  // Must add this line.
+  await windowManager.ensureInitialized();
+
+  windowManager.waitUntilReadyToShow(const WindowOptions(), () async {
+    await windowManager.show();
+    await windowManager.focus();
+    await windowManager.maximize();
+  });
+
   runApp(
     MaterialApp(
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 183, 0, 255)),
         useMaterial3: true,
       ),
-      home: const StorageApp(title: 'Controle de Estoque'),
+      home: const LoginApp(),
     )
   );
 }
 
+class LoginApp extends StatefulWidget {
+  const LoginApp({super.key});
+
+  @override
+  State<LoginApp> createState() => _LoginAppState();
+}
+
+class _LoginAppState extends State<LoginApp> {
+
+  late Map<String, TextEditingController> _formControllers;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _formControllers = {
+      'user': TextEditingController(text:'root'),
+      'password': TextEditingController(),
+    };
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+      body: Center(
+        child: SizedBox(
+          width: 600,
+          child: Card(
+            surfaceTintColor: Theme.of(context).colorScheme.onInverseSurface,
+            elevation: 10,
+            child: Padding(
+              padding: const EdgeInsets.all(50),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  for (final field in _formControllers.entries)
+                  Padding(
+                    padding: const EdgeInsets.only(bottom: 20),
+                    child: TextField(
+                      controller: field.value,
+                      obscureText: (field.key == 'password'),
+                      decoration: InputDecoration(
+                        border: const OutlineInputBorder(),
+                        labelText: loginLabels[field.key],
+                        hintText: field.value.text,
+                      ),
+                    ),
+                  ),
+                  ElevatedButton(
+                    onPressed: () => MySqlConnection.connect(
+                      ConnectionSettings(
+                        user: _formControllers['user']?.text, 
+                        password:_formControllers['password']?.text
+                      )
+                    ).then(
+                      (connection) {
+                        Navigator.pushReplacement(
+                          context, 
+                          MaterialPageRoute(builder: 
+                            (context) => StorageApp(connection: connection)
+                          )
+                        );
+                      }
+                    ), 
+                    child: const Text('Acessar')
+                  ),
+                ],
+              ),
+            )
+          )
+        ),
+      )
+    );
+  }
+}
+
 class StorageApp extends StatefulWidget {
-  const StorageApp({super.key, required this.title});
-  final String title;
+  final MySqlConnection connection;
+
+  const StorageApp({super.key, required this.connection});
 
   @override
   State<StorageApp> createState() => _StorageAppState();
@@ -40,7 +136,7 @@ class _StorageAppState extends State<StorageApp> {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
+        title: const Text('Controle de Estoque'),
         leading: IconButton(onPressed: ()=>{}, icon: const Icon(Icons.menu_rounded)),
       ),
       body: Column(
